91a9e7f3369a991771a1c7e1aa300f5ab5ad14bf
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2025-09-01 00:02:06 +0530

    Time: 8 ms (56.55%) | Memory: 15.1 MB (68.44%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n = s.length();
-        for (int len = 1; len <= n / 2; ++len) {
-            if (n % len == 0) {
-                string pattern = s.substr(0, len);
-                string repeated;
-                for (int i = 0; i < n / len; ++i) {
-                    repeated += pattern;
-                }
-                if (repeated == s) return true;
-            }
-        }
-        return false;
+        string ans2 = (s + s).substr(1, 2 * s.length() - 2);
+        return ans2.find(s) != string::npos;
     }
 };
