a75ea1939c63bfbc8c46014b0d25ac0cbf7c4a23
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2025-09-12 00:12:26 +0530

    Time: 9 ms (8.59%) | Memory: 43.3 MB (97.48%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,33 +1,29 @@
 class Solution {
-    int count = 0;
-    public int averageOfSubtree(TreeNode root){
-        if (root == null) return 0;
-        helper(root);
-        return count;
+    int ans = 0;
+
+    public int averageOfSubtree(TreeNode root) {
+        solve(root);
+        return ans;
     }
-    private int helper(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        
-        int leftsum = helper(root.left);
-        int rightsum = helper(root.right);
-        int countnode = getcount(root);
-        int sum = (leftsum + rightsum + root.val);
-        int av = sum/countnode;
-        if(av == root.val){
-            count++;
+
+    private int solve(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftSum = solve(root.left);
+        int rightSum = solve(root.right);
+
+        int sum = leftSum + rightSum + root.val;
+        int count = getCount(root); // har call pe subtree ka count nikala
+
+        if (sum / count == root.val) {
+            ans++;
         }
 
         return sum;
     }
 
-    private int getcount(TreeNode node){
-        if(node == null)return 0;
-
-        int leftc = getcount(node.left);
-        int rightc = getcount(node.right);
-
-        return leftc + rightc + 1;
+    private int getCount(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + getCount(root.left) + getCount(root.right);
     }
 }
