e98c22305fbe8426d66cec5539361573d5ec0189
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2025-09-18 01:35:28 +0530

    Time: 1 ms (16.98%) | Memory: 41.1 MB (94.81%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -0,0 +1,55 @@
+class Solution {
+    public class Pair{
+        TreeNode node;
+        TreeNode parent;
+        Pair(TreeNode node , TreeNode parent){
+            this.node = node;
+            this.parent = parent;
+        }
+    }
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        if(root == null)return false;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root,null));
+        int depth = 0;
+        int currdepthx = 0;
+        int currdepthy = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            TreeNode xparent = null;
+            TreeNode yparent = null;
+            
+            
+            for(int i = 0 ; i < size ; i++){
+                Pair p = q.poll();
+                TreeNode child = p.node;
+                TreeNode parent = p.parent;
+
+                if(child.val == x){
+                    xparent = parent;
+                    currdepthx = depth;
+                }
+                if(child.val == y){
+                    yparent = parent;
+                    currdepthy = depth;
+                }
+
+                if(child.left != null)q.add(new Pair(child.left,child));
+                if(child.right!= null)q.add(new Pair(child.right,child));
+
+
+            }
+            depth ++;
+
+            if(xparent != yparent){
+                if(currdepthx != currdepthy)return false;
+                else {
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+}
