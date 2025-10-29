ca9f0cf0fc42df50534d99848a436c021823bce2
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2025-10-30 00:58:24 +0530

    Time: 4 ms (92.60%) | Memory: 46.4 MB (17.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,47 +1,46 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            list.add(new ArrayList<>());
         }
-
-        for (int[] pre : prerequisites) {
-            graph.get(pre[1]).add(pre[0]);
+        for(int num [] : prerequisites){
+            list.get(num[1]).add(num[0]);
         }
-
-        boolean[] visited = new boolean[numCourses];
-        boolean[] path = new boolean[numCourses];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!dfs(graph, i, visited, path, stack)) {
-                    return new int[0]; // cycle detected
+        boolean vis[] = new boolean[numCourses];
+        boolean rec[] = new boolean[numCourses];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            if(!vis[i]){
+                if(!dfs(list,i,vis,rec,st)){
+                    return new int [0];
                 }
             }
         }
-
-        int[] res = new int[numCourses];
+        int ans [] = new int[numCourses];
         int idx = 0;
-        while (!stack.isEmpty()) {
-            res[idx++] = stack.pop();
+        while(!st.isEmpty()){
+            ans[idx++] = st.pop();
         }
-        return res;
+        return ans;
+
+
+
     }
+    private boolean dfs(List<List<Integer>> list , int curr , boolean[]vis , boolean[]rec, Stack<Integer> st){
+        vis[curr] = true;
+        rec[curr] = true;
 
-    private boolean dfs(List<List<Integer>> graph, int node, boolean[] visited, boolean[] path, Stack<Integer> stack) {
-        visited[node] = true;
-        path[node] = true;
-
-        for (int next : graph.get(node)) {
-            if (path[next]) return false; // cycle detected
-            if (!visited[next]) {
-                if (!dfs(graph, next, visited, path, stack)) return false;
-            }
+        for(int neigh : list.get(curr)){
+            if (!vis[neigh]) {
+                if (!dfs(list, neigh, vis, rec, st)) return false;
+        } else if (rec[neigh]) {  // yeh check bahar hona chahiye
+            return false;
         }
-
-        path[node] = false;
-        stack.push(node); // add to topo order after exploring all children
+        }
+        rec[curr] = false;
+        st.push(curr);
         return true;
+
     }
 }
