a6eea9a9f8228abd11ef049c298dc228537f67b7
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2025-12-12 15:32:14 +0530

    Time: 68 ms (62.17%) | Memory: 103.1 MB (52.05%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxEvents(int[][] events) {
+
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // stores end days
+
+        int i = 0;
+        int n = events.length;
+        int attend = 0;
+        
+        int day = events[0][0];
+
+        while(i < n || !pq.isEmpty()){
+
+            // Add all events whose start day <= current day
+            while(i < n && events[i][0] <= day){
+                pq.add(events[i][1]);
+                i++;
+            }
+
+            // Remove expired events
+            while(!pq.isEmpty() && pq.peek() < day){
+                pq.poll();
+            }
+
+            // Attend one event if possible
+            if(!pq.isEmpty()){
+                pq.poll();  // attend event with smallest end day
+                attend++;
+            }
+
+            day++;
+        }
+
+        return attend;
+    }
+}
