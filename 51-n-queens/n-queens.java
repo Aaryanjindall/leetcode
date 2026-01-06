5f273b4c4e17a43f3f176ecc45d5729a4b3a423f
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-01-06 16:57:18 +0530

    Time: 6 ms (19.15%) | Memory: 46.6 MB (46.15%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,60 +1,74 @@
 class Solution {
     List<List<String>> res = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
-        
-        String board[][] = new String[n][n];
-        //initialize
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
+        String[][] board = new String[n][n];
+
+        // initialize with "."
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 board[i][j] = ".";
             }
         }
-        nqeens(board,0);
+
+        solve(board, 0);   // start from column 0
         return res;
     }
-    private void nqeens(String board[][], int row){
-        if(row == board.length){
+
+    private void solve(String[][] board, int col) {
+        // base case: all columns filled
+        if (col == board.length) {
             res.add(construct(board));
             return;
         }
-        for(int i = 0 ; i < board.length ; i++){
-            if(isvalid(board,row,i)){
-                board[row][i] = "Q";
-                nqeens(board,row+1);
-                board[row][i] = ".";//backtrack
-            } 
+
+        // try each row in this column
+        for (int row = 0; row < board.length; row++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = "Q";
+                solve(board, col + 1);
+                board[row][col] = "."; // backtrack
+            }
         }
     }
-    private boolean isvalid(String board [][] , int row , int col){
-        //vertical check
-        for(int r = row-1 ; r >= 0 ; r--){
-            if(board[r][col].equals("Q")){
+
+    private boolean isValid(String[][] board, int row, int col) {
+
+        // same row - left side
+        for (int c = col - 1; c >= 0; c--) {
+            if (board[row][c].equals("Q")) {
                 return false;
             }
         }
-        //
-        for(int r = row-1 ,c = col-1 ; r >= 0 && c>= 0 ; r-- , c--){
-            if(board[r][c].equals("Q")){
+
+        // upper-left diagonal
+        for (int r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][c].equals("Q")) {
                 return false;
             }
         }
-        for(int r = row-1,c = col+1 ; r>= 0 && c<board.length ; r--,c++ ){
-            if(board[r][c].equals("Q")){
+
+        // lower-left diagonal
+        for (int r = row + 1, c = col - 1; r < board.length && c >= 0; r++, c--) {
+            if (board[r][c].equals("Q")) {
                 return false;
             }
         }
 
         return true;
     }
-    private List<String> construct(String[][] board){
+
+    private List<String> construct(String[][] board) {
         List<String> temp = new ArrayList<>();
-        for(int i = 0 ; i < board.length ; i++){
+
+        for (int i = 0; i < board.length; i++) {
             StringBuilder sb = new StringBuilder();
-            for(int j = 0 ; j < board.length ; j++){
+            for (int j = 0; j < board.length; j++) {
                 sb.append(board[i][j]);
             }
             temp.add(sb.toString());
         }
+
         return temp;
     }
 }
