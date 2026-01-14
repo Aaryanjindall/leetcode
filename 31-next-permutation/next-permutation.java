090b0957a8b7fd37c274400ddd89083fcc1e9d09
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-01-15 01:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (19.76%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,6 +1,32 @@
 class Solution {
-    
-    static void reverse(int[] nums, int i, int j){
+    public void nextPermutation(int[] nums) {
+        int idx = -1;
+        for(int i = nums.length-2 ; i >= 0 ; i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        for(int i = nums.length-1 ; i > idx ; i--){
+            //just greater find 
+            if(nums[i]>nums[idx]){
+                swap(nums,idx,i);
+                break;
+            }
+        }
+        reverse(nums,idx+1,nums.length-1);
+        
+    }
+    private void swap(int nums[] , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int nums[] , int i , int j){
         while(i < j){
             int temp = nums[i];
             nums[i] = nums[j];
@@ -8,41 +34,5 @@ class Solution {
             i++;
             j--;
         }
-    }
-
-    public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int idx = -1;
-
-        // Step 1: Find breakpoint
-        for(int i = n - 2; i >= 0; i--){
-            if(nums[i] < nums[i + 1]){
-                idx = i;
-                break;
-            }
-        }
-
-        // If no breakpoint found, just reverse whole array
-        if(idx == -1){
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        // Step 2: Find next greater element (rightmost greater)
-        int j = -1;
-        for(int i = n - 1; i > idx; i--){
-            if(nums[i] > nums[idx]){
-                j = i;
-                break;
-            }
-        }
-
-        // Step 3: Swap idx and j
-        int temp = nums[idx];
-        nums[idx] = nums[j];
-        nums[j] = temp;
-
-        // Step 4: Reverse the right part
-        reverse(nums, idx + 1, n - 1);
     }
 }
