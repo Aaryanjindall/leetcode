1d13a6ed1a3e8159371f047f633f4a66e2a7f7dd
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-01-27 14:07:23 +0530

    Time: 29 ms (46.96%) | Memory: 213.2 MB (69.61%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,28 +1,50 @@
 class Solution {
-    public int minZeroArray(int[] nums, int[][] queries) {
-        int l = 0, h = queries.length, ans = -1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (check(nums, queries, mid)) {
-                ans = mid;
-                h = mid - 1;
-            } else {
-                l = mid + 1;
+    public int minZeroArray(int[] nums, int[][] queries){
+        boolean alreadyZero = true;
+        for (int x : nums) {
+            if (x != 0) {
+                alreadyZero = false;
+                break;
+            }
+        }
+        if (alreadyZero) return 0;
+        int l = 0;
+        int h = queries.length-1;
+        int ans = -1;
+        while(l <= h){
+            int mid = l+(h-l)/2;
+            if(check(nums,queries,mid)){
+                ans = mid+1;
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
             }
         }
         return ans;
     }
-
-    private boolean check(int[] nums, int[][] que, int count) {
+    private boolean check(int [] nums, int [][]que , int mid){
         int n = nums.length;
-        int[] res = new int[n + 2]; // safer size
-        for (int i = 0; i < count; i++) {
-            int a = que[i][0], b = que[i][1], k = que[i][2];
+        int res[] = new int[n+1];
+        for(int i = 0; i <= mid ; i++){
+            int a = que[i][0];
+            int b = que[i][1];
+            int k = que[i][2];
             res[a] += k;
-            res[b + 1] -= k;
+            if(b+1 < n){
+                res[b+1] -= k;
+            }
         }
-        for (int i = 1; i < n; i++) res[i] += res[i - 1];
-        for (int i = 0; i < n; i++) if (res[i] < nums[i]) return false;
+
+        for(int i = 1 ; i < n ; i++){
+            res[i] += res[i-1];
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            if(res[i] < nums[i]){
+                return false;
+            }
+        }
         return true;
     }
 }
