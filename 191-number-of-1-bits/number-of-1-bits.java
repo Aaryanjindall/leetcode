6f55e7ed418528f913ed63e064d1b931a3b64454
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-01-30 15:35:55 +0530

    Time: 1 ms (31.04%) | Memory: 42.5 MB (38.35%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,12 +1,5 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count = 0;
-        for(int i = 0 ; i < 32 ; i++){
-            if((n & (1<<i)) != 0){
-                count +=1;
-            }
-        }
-        return count;
-        
+        return Integer.bitCount(n);
     }
 }
