8acc4e742f0f9cf68cffa59c45a24d022d14c14f
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-02-02 23:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.11%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -3,11 +3,10 @@ class Solution {
         int ans = start^goal;
         int count = 0;
         for(int i = 0 ; i < 32 ; i++){
-            if((ans & (1<<i)) != 0){
+            if(((ans >> i) & 1) == 1){
                 count++;
             }
         }
         return count;
-
     }
 }
