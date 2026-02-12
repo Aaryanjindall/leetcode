12eff89dc274992f1aec6e9bd5ecf643f7c4eac4
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-02-12 12:17:34 +0530

    Time: 5 ms (96.83%) | Memory: 44.7 MB (55.92%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -1,21 +1,23 @@
-import java.util.Arrays;
-
 class Solution {
     public int minimumCost(int[] cost) {
         Arrays.sort(cost);
         int cos = 0;
-        int i = cost.length - 1;
-
-        while (i >= 0) {
-            cos += cost[i];      // sabse mehenga
-            if (i - 1 >= 0) {
-                cos += cost[i - 1];   // second mehenga
+        if(cost.length < 3){
+            for(int i : cost){
+                cos += i;
             }
-            // i-2 free hai (agar exist karta hai)
-
-            i -= 3;   // next group
+            return cos;
         }
-
+        int i = cost.length-1;
+        while(i >= 0){
+            cos += cost[i];
+            if(i-1 >= 0){
+                cos += cost[i-1];
+            }
+            // if(i>=0){
+                i -= 3;
+            // }
+        }
         return cos;
     }
 }
