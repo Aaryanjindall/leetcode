fda9563d0e28db3d0e8f14e9367d18b1659d4560
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-02-19 15:09:51 +0530

    Time: 315 ms (5.48%) | Memory: 103.7 MB (5.09%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -3,41 +3,35 @@ class Solution {
         if(head == null || head.next == null){
             return true;
         }
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
-        while(fast != null && fast.next != null){
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        int size = 0;
-        ListNode temp = head;
-        while(temp != null){
-            temp = temp.next;
-            size++;
-        }
-        prev.next = null;
-        ListNode pichla = null;
-        ListNode curr = slow;
-        ListNode next = null;
+        // ListNode prev = null;
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // while(fast != null && fast.next != null){
+        //     prev = slow;
+        //     slow = slow.next;
+        //     fast = fast.next.next
+        // }
+
+        Stack<Integer> st = new Stack<>();
+        ListNode curr = head;
         while(curr != null){
-            next = curr.next;
-            curr.next = pichla;
-            pichla = curr;
-            curr = next;
+            int data = curr.val;
+            st.push(data);
+            curr = curr.next;
+            
         }
-        ListNode left = head;
-        ListNode right = pichla;
-        while(left != null && right != null){
-            if(left.val != right.val){
+        ListNode curr2 = head;
+        while(!st.isEmpty() && curr2 != null){
+            int pull = st.pop();
+            System.out.println(pull);
+            if(curr2.val != pull){
+                System.out.print(curr2.val +" "+pull);
                 return false;
             }
-            left = left.next;
-            right = right.next;
+            curr2 = curr2.next;
         }
         return true;
 
-        
+
     }
 }
