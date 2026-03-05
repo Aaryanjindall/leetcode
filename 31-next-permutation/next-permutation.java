f22cdf3e35baf44501250435b3c57be060606931
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-03-05 18:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (68.79%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,25 +1,24 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int idx = -1;
-        for(int i = nums.length-2 ; i >= 0 ; i--){
+        int j = -1;
+        int n = nums.length;
+        for(int i = n-2 ; i >= 0 ; i--){
             if(nums[i]<nums[i+1]){
-                idx = i;
+                j = i;
                 break;
             }
         }
-        if(idx == -1){
-            reverse(nums,0,nums.length-1);
+        if(j == -1){
+            reverse(nums,0,n-1);
             return;
         }
-        for(int i = nums.length-1 ; i > idx ; i--){
-            //just greater find 
-            if(nums[i]>nums[idx]){
-                swap(nums,idx,i);
+        for(int i = n-1 ; i > j; i--){
+            if(nums[i]>nums[j]){
+                swap(nums,i,j);
                 break;
             }
         }
-        reverse(nums,idx+1,nums.length-1);
-        
+        reverse(nums,j+1,n-1);
     }
     private void swap(int nums[] , int i , int j){
         int temp = nums[i];
@@ -27,7 +26,7 @@ class Solution {
         nums[j] = temp;
     }
     private void reverse(int nums[] , int i , int j){
-        while(i < j){
+        while(i<j){
             int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
