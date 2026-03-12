1c12e0bbe7d76fcbf975b4ec8150a556840c4d1a
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-03-12 12:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (79.52%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,29 +1,31 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m= obstacleGrid.length;
-        int n= obstacleGrid[0].length;
-
-        int dp[] = new int [n];
-
-        if (obstacleGrid[0][0] == 1) return 0;
-        else{
-            dp[0] = 1;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m+1][n+1];
+        for(int d[]:dp){
+            Arrays.fill(d,-1);
         }
-
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(obstacleGrid[i][j] == 1) dp[j] = 0;
-                
-                else if(j>0){
-                    dp[j] += dp[j-1];
-                }
-                
-            }
-        }
-
-        return dp[n-1];
-
-
+        return solve(grid,dp,m,n,0,0);
     }
+    private int solve(int [][] grid, int dp[][] , int m , int n , int i , int j){
+        if(i>m-1 || j>n-1){
+            return 0;
+        }
+        if(grid[i][j] == 1){
+            return 0;
+        }
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = solve(grid,dp,m,n,i+1,j);
+        int right = solve(grid,dp,m,n,i,j+1);
 
+        return dp[i][j] = right+down;
+    }
+    
 }
