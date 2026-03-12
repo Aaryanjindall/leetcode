3d5349d60ea3df35e3cdd2be1054f75ea8c45e8d
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-03-12 10:46:26 +0530

    Time: 385 ms (9.97%) | Memory: 237 MB (17.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[][] = new int[n+1][n+1];
-        for(int num[] : dp){
-            Arrays.fill(num , -1);
-
+        int dp[][] = new int [nums.length+1][nums.length+1];
+        for(int d[] : dp){
+            Arrays.fill(d,-1);
         }
         return solve(nums,dp,0,-1);
     }
-    private int solve(int nums[] , int dp[][] , int curr , int par){
-        if(curr == nums.length){
+    private int solve(int nums[] , int dp[][] , int cur,int par){
+        if(cur >= nums.length){
             return 0;
         }
-        if(dp[curr][par+1] != -1){
-            return dp[curr][par+1];
+        if(dp[cur][par+1] != -1){
+            return dp[cur][par+1];
         }
-
         int take = 0;
-        if(par == -1 || nums[curr]>nums[par]){
-            take = 1+solve(nums,dp,curr+1,curr);
+        if(par == -1 || nums[par] < nums[cur]){
+            take = 1+solve(nums,dp,cur+1,cur);
         }
-        int skip = solve(nums,dp,curr+1,par);
-        return dp[curr][par+1] = Math.max(take,skip);
-
+        int skip = solve(nums,dp,cur+1,par);
+        return dp[cur][par+1] = Math.max(take,skip);
     }
 }
