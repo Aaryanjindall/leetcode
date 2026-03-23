443eeaa06850d99a2204feda39dd87058531999d
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-03-23 12:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (38.97%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,19 +1,18 @@
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return bal(root) != -1;
+    public boolean isBalanced(TreeNode root){
+        return check(root) != -1;
     }
-    private int bal(TreeNode node){
-        if(node == null)return 0;
-          
-        int left = bal(node.left);
+    private int check(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = check(root.left);
         if(left == -1)return -1;
-        int right = bal(node.right);
+        int right = check(root.right);
         if(right == -1)return -1;
         if(Math.abs(left-right)>1){
             return -1;
         }
-        return Math.max(left,right)+1;
-
+        return 1+Math.max(left,right);
     }
-
 }
