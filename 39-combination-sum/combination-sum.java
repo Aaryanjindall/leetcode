8ede88bca40f47937147ac123979d671d700fdf0
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-04-02 18:49:28 +0530

    Time: 2 ms (95.60%) | Memory: 45.5 MB (93.94%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,21 @@
 class Solution {
-    List<List<Integer>> lister = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] cand, int tar) {
-        bt(cand,tar,new ArrayList<>(),0);
-        return lister;
+        List<List<Integer>> list = new ArrayList<>();
+        bt(list,cand,tar,0,new ArrayList<>());
+        return list;
     }
-    private void bt(int cand[] , int tar , List<Integer> list , int curr){
+    private void bt(List<List<Integer>> list , int cand[] , int tar , int curr,List<Integer> res){
         if(tar == 0){
-            lister.add(new ArrayList<>(list));
+            list.add(new ArrayList<>(res));
             return;
         }
-        if (tar < 0) return; 
-        
+        if(tar < 0) return;
         for(int i = curr ; i < cand.length ; i++){
-            list.add(cand[i]);
-            bt(cand,tar-cand[i],list,i);
-            list.remove(list.size()-1);
-
+            res.add(cand[i]);
+            bt(list,cand,tar-cand[i],i,res);
+            res.remove(res.size()-1);
         }
     }
+
+//curr hm isiliye use karte h loop m kyunki agr hm 0 se n tak krenge toh hmare pass sirf ek iteration hui lekin hmme chiye curr times iteration toh hm curr se start karte h kyunki curr pehle 0 hoga fir 0 se n fir kar hoga 1 fir 1 se n fir aise hi chalta rahgea n times iteration ke liye 
 }
