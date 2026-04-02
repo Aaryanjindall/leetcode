45e8e77a601a7474fbf5af568e431c92f1853ecd
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-04-02 17:54:18 +0530

    Time: 18 ms (82.87%) | Memory: 98 MB (74.87%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList<>();
-        backtrack(n,k,1,list,new ArrayList<>());
+        bt(list,n,k,1,new ArrayList<>());
         return list;
     }
-    private void backtrack(int n , int k ,int idx, List<List<Integer>> list,List<Integer> temp){
-        if(k == 0){
-            list.add(new ArrayList<>(temp));
+    private void bt(List<List<Integer>> list , int n , int k , int curr , List<Integer> res){
+        if(res.size()==k){
+            list.add(new ArrayList<>(res));
             return;
         }
-        for(int i = idx ; i <= n ; i++){
-            temp.add(i);
-            backtrack(n,k-1,i+1,list,temp);
-            temp.remove(temp.size()-1);
+        for(int i = curr ; i <= n ; i++){
+            res.add(i);
+            bt(list,n,k,i+1,res);
+            res.remove(res.size()-1);
         }
-
     }
 }
