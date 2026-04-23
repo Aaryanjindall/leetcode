dcfd1971e7406c75181ea19e6eb3780c1da75d18
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-04-23 19:47:52 +0530

    Time: 1670 ms (13.37%) | Memory: 42.9 MB (88.90%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -1,33 +1,29 @@
 class Solution {
-    public boolean canPartitionKSubsets(int[] nums, int k) {
+    public boolean canPartitionKSubsets(int[] nums, int k){
         int sum = 0;
-        for(int num : nums){
-            sum += num;
+        for(int x : nums){
+            sum += x;
         }
-        if(sum % k != 0){
+        if(sum%k != 0){
             return false;
         }
         int tar = sum/k;
-
-        Arrays.sort(nums);
         boolean vis[] = new boolean[nums.length + 1];
-        return bt(nums,tar,0,vis,0,k);
-            
+        return solve(nums,vis,k,tar,0,0);
     }
-    private boolean bt(int nums[] , int tar , int curr, boolean vis[] , int currsum , int k){
-        if(k == 1)return true;
-        if(curr == nums.length){
-            return false;
+    private boolean solve(int nums[] , boolean vis[] , int k , int tar , int cur,int cursum){
+        if(k == 0){
+            return true;
         }
-        if(currsum == tar){
-            return bt(nums,tar,0,vis,0,k-1);
+        if(cursum == tar){
+            return solve(nums,vis,k-1,tar,0,0);
         }
-        for(int i = curr ; i < nums.length ; i++){
-            if(vis[i] || nums[i] + currsum > tar){
+        for(int i = cur ; i < nums.length ; i++){
+            if(vis[i] || cursum + nums[i] > tar){
                 continue;
             }
             vis[i] = true;
-            if(bt(nums,tar,i+1,vis,currsum+nums[i],k)){
+            if(solve(nums,vis,k,tar,i+1,cursum+nums[i])){
                 return true;
             }
             vis[i] = false;
