1d7241eeb28b08c6fec98b355f4a0f7266efa70a
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-04-26 00:01:20 +0530

    Time: 1 ms (58.94%) | Memory: 49 MB (10.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,20 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-        left[0] = height[0];
-        for(int i = 1 ; i < n ; i++){
-            left[i] = Math.max(left[i-1],height[i]);
+        int ll [] = new int[height.length];
+        int rr [] = new int[height.length];
+        ll[0] = height[0];
+        rr[rr.length-1] = height[height.length-1];
+        for(int i = 1 ; i < height.length ; i++){
+            ll[i] = Math.max(height[i],ll[i-1]);
         }
-        right[n-1] = height[n-1];
-        for(int i = n-2 ; i>=0 ; i--){
-            right[i] = Math.max(right[i+1],height[i]);
+        for(int i = height.length-2 ; i >= 0 ; i--){
+            rr[i] = Math.max(height[i],rr[i+1]);
         }
         int trap = 0;
-        for(int i = 0 ; i < n ; i++){
-            trap += Math.min(left[i],right[i]) - height[i];
+        for(int i = 0 ; i < height.length ; i++){
+            trap += (Math.min(ll[i],rr[i])-height[i]);
         }
         return trap;
-    } 
+    }
 }
