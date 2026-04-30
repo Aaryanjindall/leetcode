080fb33591c004895704a7aa99398a362c88615c
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-04-30 23:49:56 +0530

    Time: 78 ms (12.00%) | Memory: 158.2 MB (9.17%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
         int n = nums.length;
-        int ans[]= new int[n-k+1];
+        int ans[] = new int[n-k+1];
         int idx = 0;
-        for(int i = 0 ; i < nums.length ; i++){
-            pq.add(new int[]{nums[i],i});
-            while(pq.peek()[1] <= i-k){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int i = 0 ; i < n ; i++){
+            pq.add(new int[]{i,nums[i]});
+            while(pq.peek()[0]<=i-k){
                 pq.poll();
             }
             if(i>=k-1){
-                ans[idx++] = pq.peek()[0];
+                ans[idx] = pq.peek()[1];
+                idx++;
             }
-            
         }
         return ans;
     }
