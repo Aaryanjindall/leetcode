290058b76d7928bcc3804cf2a2d50cde8dc1bff8
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-04 19:39:32 +0530

    Time: 5 ms (8.19%) | Memory: 45.8 MB (33.25%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] cand, int tar) {
+        Arrays.sort(cand);
         List<List<Integer>> list = new ArrayList<>();
-        bt(list,cand,tar,0,new ArrayList<>());
+        bt(cand,tar,list,new ArrayList<>(),0);
         return list;
     }
-    private void bt(List<List<Integer>> list , int cand[] , int tar , int curr,List<Integer> res){
+    private void bt(int cand[] , int tar , List<List<Integer>> list , List<Integer> lister , int curr){
         if(tar == 0){
-            list.add(new ArrayList<>(res));
+            list.add(new ArrayList<>(lister));
             return;
         }
-        if(tar < 0) return;
+        if(tar < 0){
+            return;
+        }
         for(int i = curr ; i < cand.length ; i++){
-            res.add(cand[i]);
-            bt(list,cand,tar-cand[i],i,res);
-            res.remove(res.size()-1);
+            lister.add(cand[i]);
+            bt(cand,tar-cand[i],list,lister,i);
+            lister.remove(lister.size()-1);
         }
     }
-
-//curr hm isiliye use karte h loop m kyunki agr hm 0 se n tak krenge toh hmare pass sirf ek iteration hui lekin hmme chiye curr times iteration toh hm curr se start karte h kyunki curr pehle 0 hoga fir 0 se n fir kar hoga 1 fir 1 se n fir aise hi chalta rahgea n times iteration ke liye 
 }
