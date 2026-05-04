1c1794a60784630fe0a9eacc8fdaec9da6bb3a26
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-05 00:03:28 +0530

    Time: 147 ms (42.47%) | Memory: 43.2 MB (30.24%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,27 @@
 class Solution {
-    public boolean exist(char[][] board, String word){
+    public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(bt(board,word,i,j,0)){
+                if(bt(word,board,i,j,n,m,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    private boolean bt(char [][] board , String word , int i , int j , int curr){
+    private boolean bt(String word , char [][] board , int i , int j , int n , int m , int curr){
         if(curr == word.length()){
             return true;
         }
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(curr)){
+        if(i < 0 || j < 0 || i >= n || j >= m || word.charAt(curr) != board[i][j]){
             return false;
         }
-        char temp = board[i][j];
+        char ch = board[i][j];
         board[i][j] = '#';
-        boolean found = bt(board,word,i+1,j,curr+1) || bt(board,word,i,j+1,curr+1) || bt(board,word,i-1,j,curr+1) || bt(board,word,i,j-1,curr+1);
-        board[i][j] = temp;
+        boolean found = bt(word,board,i+1,j,n,m,curr+1) || bt(word,board,i,j+1,n,m,curr+1) || bt(word,board,i-1,j,n,m,curr+1) || bt(word,board,i,j-1,n,m,curr+1);
+        board[i][j] = ch;
         return found;
-
     }
 }
