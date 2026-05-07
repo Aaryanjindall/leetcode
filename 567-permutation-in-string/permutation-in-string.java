a713ae7ef4923bfc3bf38a91210906dd3ced6f0c
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-07 14:56:23 +0530

    Time: 143 ms (11.65%) | Memory: 46.7 MB (20.97%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,34 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-
-        int[] freq = new int[26];
-        for (char c : s1.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        int left = 0, right = 0, count = s1.length();
-
-        while (right < s2.length()) {
-            char r = s2.charAt(right);
-
-            // if this char needed (freq > 0), decrease count
-            if (freq[r - 'a'] > 0) count--;
-            freq[r - 'a']--;
-            right++;
-
-            // when window size matches
-            if (count == 0) return true;
-
-            // if window size exceeds, move left pointer
-            if (right - left >= s1.length()) {
-                char l = s2.charAt(left);
-                if (freq[l - 'a'] >= 0) count++;
-                freq[l - 'a']++;
-                left++;
+        int s1l = s1.length();
+        int s2l = s2.length();
+        int n = s2l-s1l;
+        for(int i = 0 ; i <= n ; i++){
+            if(check(s2.substring(i,i+s1l),s1)){
+                return true;
             }
         }
-
         return false;
+    }
+    private boolean check(String s , String p){
+        int fre[] = new int[26];
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            fre[ch-'a']++;
+            char cha = p.charAt(i);
+            fre[cha-'a']--;
+        }
+        for(int i : fre){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
