b60a152402a667091acd61caf7223b8d1f5a9dba
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-14 15:36:21 +0530

    Time: 12 ms (13.85%) | Memory: 46.1 MB (97.11%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,49 +1,52 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k){
         List<List<int[]>> graph = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             graph.add(new ArrayList<>());
         }
-        for(int it[] : flights){
-            int u = it[0];
-            int v = it[1];
-            int w = it[2];
+        for(int f[] : flights){
+            int u = f[0];
+            int v = f[1];
+            int w = f[2];
             graph.get(u).add(new int[]{v,w});
         }
-        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        pq.add(new int[]{0,src,0});
-        int[][] dist = new int[n][k + 2];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        int dist[][] = new int[n][k+2];
+        for(int d[] : dist){
+            Arrays.fill(d,(int)1e9);
         }
         dist[src][0] = 0;
-
-
-
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.add(new int[]{0,src,0});
         while(!pq.isEmpty()){
-            int top [] = pq.poll();
-            int d = top[0];
-            int u = top[1];
-            int stop = top[2];
-            if (stop > k) continue;
-            for(int neigh[] : graph.get(u)){
-                int v = neigh[0];
-                int w = neigh[1];
-                if(dist[u][stop]+w<dist[v][stop+1]){
-                    dist[v][stop+1] = dist[u][stop]+w;
-                    pq.add(new int[]{dist[v][stop+1],v,stop+1});
-                }
+            int curr[] = pq.poll();
+            int w = curr[0];
+            int u = curr[1];
+            int stop = curr[2];
+            if(u == dst){
+                return w;
             }
+            if(stop > k+1){
+                continue;
+            }
+            for(int neigh[] : graph.get(u)){
+
+    int next = neigh[0];
+    int weight = neigh[1];
+
+    if(stop + 1 <= k + 1 &&
+       w + weight < dist[next][stop+1]){
+
+        dist[next][stop+1] = w + weight;
+
+        pq.add(new int[]{
+            w + weight,
+            next,
+            stop + 1
+        });
+    }
+}
         }
-
-
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0 ; i <= k+1 ; i++){
-            ans = Math.min(ans, dist[dst][i]);
-        }
-
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-
-
+        return -1;
+        
     }
 }
