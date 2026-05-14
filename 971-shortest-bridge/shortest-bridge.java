0145d8f84def568f020ac0852f067addb5c5430e
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-14 13:28:38 +0530

    Time: 8 ms (93.93%) | Memory: 46.8 MB (89.17%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -1,70 +1,64 @@
 class Solution {
-    int steps = 0;
     public int shortestBridge(int[][] grid) {
-        // return 1;
         int n = grid.length;
-        boolean vis[][] = new boolean[n][n];
-        outer:
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == 1){
-                    dfs(grid,vis,i,j);
-                    break outer;
-                }
-            }
-        }
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                System.out.print(vis[i][j]+" ");
-            }
-            System.out.println();
-        }
+        int m = grid[0].length;
+        boolean vis[][] = new boolean[n][m];
+        boolean flag = false;
         Queue<int[]> q = new LinkedList<>();
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(vis[i][j]){
-                    q.add(new int[]{i,j});
+            if(flag)break;
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1){
+                    dfs(grid,vis,q,i,j);
+                    flag = true;
+                    break;
                 }
             }
         }
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
+        int steps = 0;
         while(!q.isEmpty()){
             int size = q.size();
-            while(size-- > 0){
+            while(size-->0){
                 int curr[] = q.poll();
-                int r = curr[0];
-                int c = curr[1];
-                for(int k = 0 ; k < 4 ; k++){
-                    int nr = r + dx[k];
-                    int nc = c + dy[k];
-                
-                if(nr >= 0 && nc >= 0 && nr < n && nc < n && !vis[nr][nc]){
-                    if(grid[nr][nc] == 1){
-                        return steps;
+                int x = curr[0];
+                int y = curr[1];
+                for(int i = 0 ; i < 4 ; i++){
+                    int nr = x+dx[i];
+                    int nc = y+dy[i];
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < m){
+                        if(!vis[nr][nc]){
+                            if(grid[nr][nc] == 1){
+                                return steps;
+                            }
+                            vis[nr][nc] = true;
+                            q.add(new int[]{nr,nc});
+                        }
+                        
                     }
-                    vis[nr][nc] = true;
-                    q.add(new int[]{nr,nc});
-                    
-                }
                 }
             }
             steps++;
         }
-        
-
-        return -1;
+        return steps;
 
     }
-    private void dfs(int[][]grid , boolean vis[][] , int i , int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid.length || vis[i][j] || grid[i][j] == 0){
+    private void dfs(int[][] grid , boolean vis[][] , Queue<int[]> q , int i , int j){
+        if(i<0 || j < 0 || i >= grid.length || j >= grid[0].length){
+            return;
+        }
+        if(vis[i][j]){
+            return;
+        }
+        if(grid[i][j] == 0){
             return;
         }
         vis[i][j] = true;
-        dfs(grid,vis,i+1,j);
-        dfs(grid,vis,i,j+1);
-        dfs(grid,vis,i-1,j);
-        dfs(grid,vis,i,j-1);
+        q.offer(new int[]{i,j});
+        dfs(grid,vis,q,i+1,j);
+        dfs(grid,vis,q,i-1,j);
+        dfs(grid,vis,q,i,j+1);
+        dfs(grid,vis,q,i,j-1);
     }
-    
 }
