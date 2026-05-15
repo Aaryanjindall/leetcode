94d793ffe91e1b9a2355d065394215b45b965d74
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-15 15:48:27 +0530

    Time: 81 ms (34.21%) | Memory: 43 MB (57.01%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -3,34 +3,32 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int maxgold = 0;
+        boolean vis[][] = new boolean[n][m];
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j] != 0){
-                    maxgold = Math.max(maxgold,dfs(grid,i,j));
+                    int gold = dfs(grid,vis,i,j,n,m);
+                    maxgold = Math.max(gold,maxgold);
                 }
             }
         }
-
         return maxgold;
-
     }
-    private int dfs(int grid[][] , int i , int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+    private int dfs(int [][]grid , boolean vis[][] , int i , int j , int n , int m){
+        if(i<0 || j < 0 || i > n-1 || j > m-1 || grid[i][j] == 0){
             return 0;
         }
-
-        int gold = grid[i][j];
-        grid[i][j] = 0;
-        int up = dfs(grid,i-1,j);
-        int down = dfs(grid,i,j-1);
-        int right = dfs(grid,i+1,j);
-        int left = dfs(grid,i,j+1);
-
-        grid[i][j] = gold;
-
-        return gold + Math.max(Math.max(up,down),Math.max(right,left));
+        if(vis[i][j]){
+            return 0;
+        }
+        vis[i][j] = true;
+        int left = grid[i][j]+dfs(grid,vis,i-1,j,n,m);
+        int right = grid[i][j]+dfs(grid,vis,i+1,j,n,m);
+        int top = grid[i][j]+dfs(grid,vis,i,j-1,n,m);
+        int bottom = grid[i][j]+dfs(grid,vis,i,j+1,n,m);
+        int gold = Math.max(Math.max(left,right),Math.max(top,bottom));
+        vis[i][j] = false;
+        return gold;
         
-        
-
     }
 }
