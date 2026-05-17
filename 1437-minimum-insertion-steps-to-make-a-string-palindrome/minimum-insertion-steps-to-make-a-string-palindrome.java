fe5743ace7f00eac4606cc73df6c712ffbdc18d2
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-17 14:12:01 +0530

    Time: 16 ms (95.86%) | Memory: 47.9 MB (11.49%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int minInsertions(String s) {
+    public int minInsertions(String s){
         int n = s.length();
         int dp[][] = new int[n+1][n+1];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        for(int d[] : dp){
+            Arrays.fill(d,-1);
         }
-        return solve(s,0,n-1,dp);
+        return solve(dp,s,0,s.length()-1);
     }
-    private int solve(String s , int i , int j , int dp[][]){
-        if(i > j){
+    private int solve(int dp[][] , String s , int i , int j){
+        if(i >= j){
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = solve(s,i+1,j-1,dp);
+            return dp[i][j] = solve(dp,s,i+1,j-1);
         }
-        int left = solve(s,i,j-1,dp);
-        int right = solve(s,i+1,j,dp);
 
-        return dp[i][j] = 1+ Math.min(left,right);
+        return dp[i][j] = 1+Math.min(solve(dp,s,i+1,j),solve(dp,s,i,j-1));
+        
     }
-
 }
