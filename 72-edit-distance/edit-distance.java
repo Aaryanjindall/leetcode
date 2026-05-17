441cdf249f3105535bf910e18a510c87b056eb6e
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-17 13:19:22 +0530

    Time: 3 ms (98.52%) | Memory: 47.3 MB (32.65%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,29 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        int dp[][] = new int [word1.length()+1][word2.length()+1];
         for(int d[] : dp){
             Arrays.fill(d,-1);
         }
         return solve(dp,word1,word2,0,0);
+        
     }
-    private int solve(int dp[][] , String word1 , String word2 , int i , int j){
-        if(i == word1.length()){
-            return word2.length()-j;
+    private int solve(int dp[][],String word1 , String word2 , int s1 , int s2 ){
+        if(s1 == word1.length()){
+            return word2.length()-s2;
         }
-        if(j == word2.length()){
-            return word1.length()-i;
+        if(s2 == word2.length()){
+            return word1.length()-s1;
         }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[s1][s2] != -1){
+            return dp[s1][s2];
         }
-        if(word1.charAt(i) == word2.charAt(j)){
-            return dp[i][j] = solve(dp,word1,word2,i+1,j+1);
+        if(word1.charAt(s1) == word2.charAt(s2)){
+            return dp[s1][s2] = solve(dp,word1,word2,s1+1,s2+1);
         }
-        int insert = solve(dp,word1,word2,i+1,j+1);
-        int delete = solve(dp,word1,word2,i+1,j);
-        int replace = solve(dp,word1,word2,i,j+1);
-
-        return dp[i][j] = 1+Math.min(insert,Math.min(delete,replace));
+        int insert = solve(dp,word1,word2,s1,s2+1);
+        int delete = solve(dp,word1,word2,s1+1,s2);
+        int replace = solve(dp,word1,word2,s1+1,s2+1);
+        return dp[s1][s2] = 1+Math.min(insert,Math.min(delete,replace));
     }
 }
