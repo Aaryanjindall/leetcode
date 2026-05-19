b2fc66025c48f3cbee37e73786877f8c7eb0f4d7
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-19 13:23:19 +0530

    Time: 25 ms (77.89%) | Memory: 54.8 MB (46.04%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        for(int [] point : points){
-            int x = point[0];
-            int y = point[1];
-            int d = (x - 0)*(x-0)+(y-0)*(y - 0);
-            pq.add(new int[]{d,x,y});
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        for(int p[] : points){
+            int x = p[0];
+            int y = p[1];
+            int d = (x-0)*(x-0)+(y-0)*(y-0);
+            pq.add(new int[]{x,y,d});
         }
-
-        int [][] ans = new int[k][2];
         int idx = 0;
-        while(idx < k && !pq.isEmpty()){
-            int [] top = pq.poll();
-            int x = top[1];
-            int y = top[2];
-            System.out.println(x +" " + y);
-            ans[idx][0] = x;
-            ans[idx][1] = y;
+        int ans[][] = new int [k][2];
+        while(!pq.isEmpty() && idx < k){
+            int curr[] = pq.poll();
+            int currx = curr[0];
+            int curry = curr[1];
+            ans[idx][0] = currx;
+            ans[idx][1] = curry;
             idx++;
         }
         return ans;
-
-
+        
     }
 }
