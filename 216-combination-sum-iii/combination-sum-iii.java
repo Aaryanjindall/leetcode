ff4d9e5ebd67ea4c615148ccd870212554e8dc33
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-25 14:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (46.66%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> list = new ArrayList<>();
-        backtrack(list,1,k,n,new ArrayList<>());
+        bt(list,k,n,1,new ArrayList<>());
         return list;
     }
-    private void backtrack(List<List<Integer>> list , int curr , int k , int tar , List<Integer> ans){
-        if(k == 0){
-            if(tar == 0){
-                list.add(new ArrayList<>(ans));
+    private void bt(List<List<Integer>> list , int k , int n , int curr , List<Integer> lister){
+        if (k == 0) {
+            if (n == 0) {
+                list.add(new ArrayList<>(lister));
             }
+            return;
         }
-        for(int i = curr ; i <=9  ; i++){
-            ans.add(i);
-            backtrack(list,i+1,k-1,tar-i,ans);
-            ans.remove(ans.size()-1);
+        for(int i = curr ; i <= 9 ; i++){
+            lister.add(i);
+            bt(list,k-1,n-i,i+1,lister);
+            lister.remove(lister.size()-1);
         }
     }
 }
