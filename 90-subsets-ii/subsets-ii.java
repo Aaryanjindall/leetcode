1ebda2993e1afd0f132a3ec8d8e13b4ca8e95845
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-26 11:41:59 +0530

    Time: 4 ms (16.70%) | Memory: 45.2 MB (41.15%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        HashSet<List<Integer>> hs = new HashSet<>();
-        bt(list,0,nums,new ArrayList<>(),hs);
-        return list;
+        Set<List<Integer>> list = new HashSet<>();
+        bt(nums,list,0,new ArrayList<>());
+        return new ArrayList<>(list);
     }
-    private void bt(List<List<Integer>> list, int curr , int nums[] , List<Integer> lister,HashSet<List<Integer>> hs){
-        if(!hs.contains(lister)){
-            hs.add(new ArrayList<>(lister));
-            list.add(new ArrayList<>(lister));
-        }
+    private void bt(int nums[] , Set<List<Integer>> list , int curr , List<Integer> lister){
+        list.add(new ArrayList<>(lister));
         for(int i = curr ; i < nums.length ; i++){
             lister.add(nums[i]);
-            bt(list,i+1,nums,lister,hs);
+            bt(nums,list,i+1,lister);
             lister.remove(lister.size()-1);
         }
     }
-}
+} 
