a97ebe068d5786ef6b724bddc78e586461793ac7
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-26 15:42:34 +0530

    Time: 49 ms (5.51%) | Memory: 46.9 MB (12.19%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxVowels(String s, int k) {
+        HashMap<Character,Integer> hm = new HashMap<>();
         int n = s.length();
-        String vowels = "aeiou";
-        int max = 0;
-        int vc = 0;
+        int vowels = 0;
+        String vowel = "aeiou";
         int j = 0;
+        int max = 0;
         for(int i = 0 ; i < n ; i++){
             char ch = s.charAt(i);
-            if(vowels.indexOf(ch) != -1){
-                vc++;
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+            if(vowel.indexOf(ch) != -1){
+                vowels++;
             }
-            while(i-j+1 > k){
+            while(i-j+1 == k){
+                max = Math.max(max,vowels);
                 char cha = s.charAt(j);
-                if(vowels.indexOf(cha) != -1){
-                    vc--;
+                hm.put(cha,hm.get(cha)-1);
+                if(vowel.indexOf(cha) != -1){
+                    vowels--;
                 }
                 j++;
-            }
-            if(i-j+1 == k){
-                max = Math.max(vc,max);
             }
         }
         return max;
