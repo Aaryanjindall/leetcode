9bbb588a8b99365da6bc937035465ab446fa40cc
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-28 15:31:05 +0530

    Time: 8 ms (21.83%) | Memory: 51.2 MB (22.41%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
+        int currmax = nums[0];
         int max = nums[0];
+        int currmin = nums[0];
         int min = nums[0];
-        int maxcurr = nums[0];
-        int mincurr = nums[0];
         int total = nums[0];
         for(int i = 1 ; i < nums.length ; i++){
-            maxcurr = Math.max(nums[i],nums[i]+maxcurr);
-            max = Math.max(max,maxcurr);
-            mincurr = Math.min(nums[i],nums[i]+mincurr);
-            min = Math.min(min,mincurr);
             total += nums[i];
+            currmax = Math.max(nums[i],currmax+nums[i]);
+            max = Math.max(currmax,max);
+            currmin = Math.min(nums[i],currmin+nums[i]);
+            min = Math.min(currmin,min);
         }
         if(max < 0){
             return max;
