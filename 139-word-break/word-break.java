1b7ee51087253e718948dd955450de7dd3e79853
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-29 14:39:40 +0530

    Time: 6 ms (67.01%) | Memory: 46.1 MB (46.39%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,42 +1,24 @@
 class Solution {
-
     public boolean wordBreak(String s, List<String> wordDict) {
-
         HashSet<String> hs = new HashSet<>(wordDict);
-
-        Boolean dp[] = new Boolean[s.length()];
-
-        return solve(dp, hs, s, 0);
+        Boolean []dp = new Boolean[s.length()];
+        return solve(dp,hs,wordDict,s,0);
     }
-
-    private boolean solve(Boolean dp[],
-                          HashSet<String> hs,
-                          String s,
-                          int curr) {
-
-        // Reached end
-        if(curr == s.length()) {
+    private boolean solve(Boolean []dp,HashSet<String> hs , List<String> list ,String s, int curr){
+        if(curr == s.length()){
             return true;
         }
-
-        // Already computed
-        if(dp[curr] != null) {
+        if(dp[curr] != null){
             return dp[curr];
         }
-
-        // Try all substrings
-        for(int i = curr; i < s.length(); i++) {
-
-            String part = s.substring(curr, i + 1);
-
-            if(hs.contains(part)) {
-
-                if(solve(dp, hs, s, i + 1)) {
+        for(int i = curr ; i < s.length() ; i++){
+            String part = s.substring(curr,i+1);
+            if(hs.contains(part)){
+                if(solve(dp,hs,list,s,i+1)){
                     return dp[curr] = true;
                 }
             }
         }
-
         return dp[curr] = false;
     }
 }
