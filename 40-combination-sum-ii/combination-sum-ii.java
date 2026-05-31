9c2eb1db696cf04dcb28681c13d8b94f61b70870
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-05-31 23:49:34 +0530

    Time: 7 ms (35.65%) | Memory: 45 MB (77.51%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -2,21 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] cand, int tar) {
         Arrays.sort(cand);
         List<List<Integer>> list = new ArrayList<>();
-        bt(list,cand,tar,0,new ArrayList<>());
+        bt(cand,tar,list,0,new ArrayList<>());
         return list;
     }
-    private void bt(List<List<Integer>> list , int cand[] , int tar , int curr , List<Integer> lister){
+    private void bt(int cand[] , int tar , List<List<Integer>> list , int cur , List<Integer> lister){
         if(tar == 0){
             list.add(new ArrayList<>(lister));
             return;
         }
-        if(tar <0){
+        if(tar < 0){
             return;
         }
-        for(int i = curr ; i < cand.length ; i++){
-            if(i>curr && cand[i] == cand[i-1])continue;
+        for(int i = cur ; i < cand.length ; i++){
+            if(i > cur && cand[i] == cand[i-1]){
+                continue;
+            }
             lister.add(cand[i]);
-            bt(list,cand,tar-cand[i],i+1,lister);
+            bt(cand,tar-cand[i],list,i+1,lister);
             lister.remove(lister.size()-1);
         }
     }
