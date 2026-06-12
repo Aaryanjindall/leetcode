fe3400d618ad4e3650dc15dbad7d94584c7f61f0
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-12 12:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (87.87%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,38 +1,37 @@
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode deleteNode(TreeNode root, int key) { 
         if(root == null){
             return null;
         }
-        if(root.val < key){
-            root.right = deleteNode(root.right,key);
-            return root;
-        }
-        else if(root.val > key){
+        if(key < root.val){
             root.left = deleteNode(root.left,key);
             return root;
         }
+        else if(key > root.val){
+            root.right = deleteNode(root.right,key);
+            return root;
+        }
         else{
+            //one leaf
             if(root.left == null && root.right == null){
                 return null;
             }
-            else if(root.left == null){
+            if(root.left == null){
                 return root.right;
             }
-            else if(root.right == null){
+            if(root.right == null){
                 return root.left;
             }
             else{
-                TreeNode succ = find(root.right);
+                TreeNode succ = inorder(root.right);
                 root.val = succ.val;
                 root.right = deleteNode(root.right,succ.val);
-                
+                return root;
             }
         }
-        return root;
-        
     }
-    private TreeNode find(TreeNode root){
-        TreeNode curr = root;
+    private TreeNode inorder(TreeNode node){
+        TreeNode curr = node;
         while(curr.left != null){
             curr = curr.left;
         }
