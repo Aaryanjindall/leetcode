76ddbfcdb1c15052a72321c01bea900373ddb65c
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-13 17:40:16 +0530

    Time: 4 ms (23.23%) | Memory: 47.4 MB (91.74%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,25 +1,24 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int ll[] = new int[n];
-        int rr[] = new int[n];
+        int []ll = new int[n];
+        int []rr = new int[n];
         Arrays.fill(ll,1);
         Arrays.fill(rr,1);
         for(int i = 1 ; i < n ; i++){
             if(ratings[i]>ratings[i-1]){
-                ll[i] += ll[i-1];
+                ll[i] = ll[i-1]+1;
             }
         }
-        for(int i = n-2 ; i >= 0 ; i--){
-            if(ratings[i]>ratings[i+1]){
-                rr[i] += rr[i+1];
+        for(int i = n-2 ; i>= 0 ; i--){
+            if(ratings[i] >ratings[i+1]){
+                rr[i] = rr[i+1]+1;
             }
         }
-        int sum = 0;
+        int ans = 0;
         for(int i = 0 ; i < n ; i++){
-            System.out.println(ll[i]+" "+rr[i]);
-            sum += Math.max(ll[i],rr[i]);
+            ans += Math.max(ll[i],rr[i]);
         }
-        return sum;
+        return ans;
     }
 }
