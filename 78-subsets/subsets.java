0b8fcfbb5eb41a82a5e0791777d64d5c0e9765fd
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-14 20:36:59 +0530

    Time: 1 ms (88.72%) | Memory: 44.2 MB (38.72%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        bt(nums,list,0,new ArrayList<>());
+        bt(list,nums,0,new ArrayList<>());
         return list;
     }
-    private void bt(int nums[] , List<List<Integer>> list , int curr , List<Integer> lister){
+    private void bt(List<List<Integer>> list ,int nums[], int curr , List<Integer> lister){
         list.add(new ArrayList<>(lister));
         for(int i = curr ; i < nums.length ; i++){
             lister.add(nums[i]);
-            bt(nums,list,i+1,lister);
+            bt(list,nums,i+1,lister);
             lister.remove(lister.size()-1);
         }
     }
-} 
+}
