f03e23332868cdfee76f91cbadca47c407196f01
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-14 16:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (13.30%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -4,17 +4,20 @@ class Solution {
         bt(list,k,n,1,new ArrayList<>());
         return list;
     }
-    private void bt(List<List<Integer>> list , int k , int n , int curr , List<Integer> lister){
-        if (k == 0) {
-            if (n == 0) {
+    private void bt(List<List<Integer>> list , int k , int n ,int cur, List<Integer> lister){
+        if(k == 0){
+            if(n == 0){
                 list.add(new ArrayList<>(lister));
+                return;
             }
+        }
+        if(n<0){
             return;
         }
-        for(int i = curr ; i <= 9 ; i++){
+        for(int i = cur ; i <= 9 ; i++){
             lister.add(i);
             bt(list,k-1,n-i,i+1,lister);
             lister.remove(lister.size()-1);
-        }
+        }       
     }
 }
