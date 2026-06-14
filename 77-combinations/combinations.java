9b92c553470843074c1f72b1efb9957b6ac4a871
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-15 00:19:47 +0530

    Time: 17 ms (86.50%) | Memory: 97.7 MB (91.49%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList<>();
-        bt(list,n,k,1,new ArrayList<>());
+        bt(list,n,k,n,new ArrayList<>());
         return list;
     }
     private void bt(List<List<Integer>> list , int n , int k , int cur , List<Integer> lister){
@@ -9,9 +9,9 @@ class Solution {
             list.add(new ArrayList<>(lister));
             return;
         }
-        for(int i = cur ; i <= n ; i++){
+        for(int i = cur ; i >= 1 ; i--){
             lister.add(i);
-            bt(list,n,k-1,i+1,lister);
+            bt(list,n,k-1,i-1,lister);
             lister.remove(lister.size()-1);
         }
 
