8edb73f4428aed257219811ff3cfa9738df71d6e
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-17 00:36:10 +0530

    Time: 14 ms (93.68%) | Memory: 46.4 MB (68.85%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int sum1 = 0;
-        int sum2 = 0;
         int n = s1.length();
         int m = s2.length();
         int dp[][] = new int[n+1][m+1];
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                }
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = s1.charAt(i - 1) +dp[i-1][j-1];
+                    dp[i][j] = s1.charAt(i-1)+dp[i-1][j-1];
                 }
                 else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
@@ -19,13 +14,15 @@ class Solution {
             }
         }
         int lcs = dp[n][m];
-        for(char ch : s1.toCharArray()){
-            sum1 += (int)ch;
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            sum1 += (int)s1.charAt(i);
         }
-        for(char ch : s2.toCharArray()){
-            sum2 += (int)ch;
+        for(int i = 0 ; i < m ; i++){
+            sum2 += (int)s2.charAt(i);
         }
-        System.out.println(sum1+" "+sum2+ " "+lcs);
         return sum1+sum2-2*lcs;
+
     }
 }
