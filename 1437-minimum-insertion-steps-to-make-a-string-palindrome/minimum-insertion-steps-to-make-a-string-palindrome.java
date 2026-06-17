e82b2530726ffefe0348257fdaffcafe53037570
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-17 11:46:00 +0530

    Time: 19 ms (85.69%) | Memory: 47.6 MB (28.51%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int minInsertions(String s){
+    public int minInsertions(String s) {
         int n = s.length();
         int dp[][] = new int[n+1][n+1];
         for(int d[] : dp){
             Arrays.fill(d,-1);
-        }
-        return solve(dp,s,0,s.length()-1);
+        }   
+        return solve(dp,s,0,n-1);
     }
     private int solve(int dp[][] , String s , int i , int j){
-        if(i >= j){
+        if(i>j){
             return 0;
         }
         if(dp[i][j] != -1){
@@ -17,8 +17,8 @@ class Solution {
         if(s.charAt(i) == s.charAt(j)){
             return dp[i][j] = solve(dp,s,i+1,j-1);
         }
-
-        return dp[i][j] = 1+Math.min(solve(dp,s,i+1,j),solve(dp,s,i,j-1));
-        
+        int left = solve(dp,s,i+1,j);
+        int right = solve(dp,s,i,j-1);
+        return dp[i][j] = 1+Math.min(left,right);
     }
 }
