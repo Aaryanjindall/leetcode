cbfa27b67a4623a4a475183da0f4b77bd04e49eb
AARYAN JINDAL <157559123+Aaryanjindall@users.noreply.github.com>
2026-06-17 12:14:28 +0530

    Time: 29 ms (23.22%) | Memory: 53.1 MB (6.93%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int countSubstrings(String s) {
-        int dp[][] = new int[1000][1000];
-        for(int d[] : dp){
+    public int countSubstrings(String s){
+        int n = s.length();
+        int dp[][] = new int[n+1][n+1];
+        for(int d[]: dp){
             Arrays.fill(d,-1);
         }
         int count = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            for(int j = i ; j < s.length() ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
                 if(solve(dp,i,j,s) == 1){
                     count++;
                 }
@@ -27,6 +28,6 @@ class Solution {
         if(s.charAt(i) == s.charAt(j)){
             return dp[i][j] = solve(dp,i+1,j-1,s);
         }
-        return dp[i][j] = 0;
+        return dp[i][j] = -1;
     }
 }
